35f602d6e9662c03243bc8f04c1ff90548a64846
SadafAmininiiiaa <aminicomputer96@gmail.com>
2022-12-29 21:28:39 +0330

    first commit

---
+++ constants/data.js
@@ -0,0 +1,116 @@
+const userData = {
+  githubUsername: "manuarora700",
+  name: "sadaf Amininia",
+  designation: "Front-end Developer",
+  avatarUrl: "/avatar.png",
+  email: "sadaf.amininiaa@gmail.co",
+  phone: "+98 9108730180",
+  address: "Tehran, Iran.",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a Frontend  developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Frontend  Developer",
+      company: "Tapsell",
+      year: "2021",
+      companyLink: "https://tapsell.ir/",
+      desc: "We take  pride in bringing advertisers and publishers together while providing not only the best offers on variety of tags and verticals and high-grade traffic to promote but also offerings of  high proficiency with well-designed creatives and the leading-edge monetization tools available on the market.",
+    },
+    {
+      title: "Founder and Developer",
+      company: "TailwindMasterKit",
+      year: "2021",
+      companyLink: "https://tailwindmasterkit.com",
+      desc: "Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.",
+    },
+    {
+      title: "Founder and Developer",
+      company: "Algochurn",
+      year: "2021",
+      companyLink: "https://algochurn.com",
+      desc: "Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.",
+    },
+    {
+      title: "Internship",
+      company: "Intel Corporation",
+      year: "2019",
+      companyLink: "https://intel.com",
+      desc: "Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.",
+    },
+    {
+      title: "Graduation",
+      company: "UPES, Dehradun.",
+      year: "2019",
+      companyLink: "https://upes.co.in",
+      desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
+    },
+    {
+      title: "High School",
+      company: "Delhi Public School, Jaipur.",
+      year: "2015",
+      companyLink: "https://upes.co.in",
+      desc: "Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/girl._.codee/",
+    twitter: "https://twitter.com/sadafamininiia",
+    linkedin: "https://www.linkedin.com/in/sadafamininia/",
+    github: "https://github.com/sadafamininia99",
+  },
+};
+
+export default userData;
